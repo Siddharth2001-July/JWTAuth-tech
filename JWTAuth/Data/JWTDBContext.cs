44b8c50d71f005b684471bac0d7f3f64b4493862
Siddharth <kambojsiddharth199@gmail.com>
2023-04-11 15:29:43 +0530

    Add project files.

---
+++ JWTAuth/Data/JWTDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuth.Data
+{
+    public class JWTDBContext : IdentityDbContext
+    {
+        public JWTDBContext(DbContextOptions<JWTDBContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
